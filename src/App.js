bf78c4db7981def4e115225cfbdbe8f49f021879
Rakibul santo <SANTODON176@gmail.com>
2021-09-19 22:32:11 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <LoadData></LoadData>
+ 
     </div>
   );
 }
+
+function LoadData(){
+const [allData,setData] = useState([]);
+
+useEffect(()=>{
+  fetch('https://www.themealdb.com/api/json/v1/1/search.php?s')
+.then(res=> res.json())
+.then(data => setData(data.meals))
+},[]
+)
+return(
+  <div id='mainDiv'>
+   {
+    allData.map(datas => <MealDb  strMealThumb={datas.strMealThumb} strMeal={datas.strMeal} strTags={datas.strTags} strArea={datas.strArea}></MealDb>)
+   }
+  </div>
+)
+
+}
+
+function MealDb(props){
+  return(
+    <div className='mealdb'>
+      <img src={props.strMealThumb} alt="" />
+      <h2>{props.strMeal}</h2>
+      <h2>{props.strTags}</h2>
+      <h4>{props.strArea}</h4>
+    </div>
+  )
+}
+
+
 
 export default App;
